6a363bf91faca65964195664c67b977d1fa5e897
gilles20393 <gilles1996@outlook.com>
2024-06-13 18:31:05 +0200

    feat: Add DefaultFirebaseOptions class with currentPlatform getter
    The commit adds the `DefaultFirebaseOptions` class with a `currentPlatform` getter. This getter returns a `FirebaseOptions` object with the default configuration values for the Firebase project.

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+        apiKey: "AIzaSyDbmr4sEOYtDxPXE8smthQhgoSILtF-uHw",
+        authDomain: "flutter-b2483.firebaseapp.com",
+        projectId: "flutter-b2483",
+        storageBucket: "flutter-b2483.appspot.com",
+        messagingSenderId: "702932366935",
+        appId: "1:702932366935:web:495ee10a05275313c55512");
+  }
+}
