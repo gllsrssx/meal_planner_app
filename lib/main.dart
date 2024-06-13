8c76a62386856c6958412d40bd61d3fd630d0e4a
gilles20393 <gilles1996@outlook.com>
2024-06-13 18:30:59 +0200

    feat: Add FamilySharingScreen widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,269 @@
+import 'package:firebase_auth/firebase_auth.dart'
+    hide PhoneAuthProvider, EmailAuthProvider;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_localizations/firebase_ui_localizations.dart';
+import 'package:firebase_ui_oauth_apple/firebase_ui_oauth_apple.dart';
+import 'package:firebase_ui_oauth_facebook/firebase_ui_oauth_facebook.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
+import 'package:firebase_ui_oauth_twitter/firebase_ui_oauth_twitter.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'config.dart';
+// import 'decorations.dart';
+import 'firebase_options.dart';
+
+final actionCodeSettings = ActionCodeSettings(
+  url: 'flutter-b2483.firebaseapp.com',
+  handleCodeInApp: true,
+  androidMinimumVersion: '1',
+  androidPackageName: 'io.flutter.plugins.firebase_ui.firebase_ui_example',
+  iOSBundleId: 'io.flutter.plugins.firebaseUiExample',
+);
+final emailLinkProviderConfig = EmailLinkAuthProvider(
+  actionCodeSettings: actionCodeSettings,
+);
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
+  FirebaseUIAuth.configureProviders([
+    EmailAuthProvider(),
+    emailLinkProviderConfig,
+    PhoneAuthProvider(),
+    GoogleProvider(clientId: Config.googleClientId),
+    AppleProvider(),
+    FacebookProvider(clientId: Config.facebookClientId),
+    TwitterProvider(
+      apiKey: Config.twitterApiKey,
+      apiSecretKey: Config.twitterApiSecretKey,
+      redirectUri: Config.twitterRedirectUri,
+    ),
+  ]);
+
+  runApp(const FirebaseAuthUIExample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LabelOverrides extends DefaultLocalizations {
+  const LabelOverrides();
 
-  // This widget is the root of your application.
+  @override
+  String get emailInputLabel => 'Enter your email';
+}
+
+class FirebaseAuthUIExample extends StatelessWidget {
+  const FirebaseAuthUIExample({super.key});
+
+  String get initialRoute {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return switch (user) {
+      null => '/',
+      // User(emailVerified: false, email: final String _) => '/verify-email',
+      _ => '/profile',
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    final buttonStyle = ButtonStyle(
+      padding: WidgetStateProperty.all(const EdgeInsets.all(12)),
+      shape: WidgetStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+    final mfaAction = AuthStateChangeAction<MFARequired>(
+      (context, state) async {
+        final nav = Navigator.of(context);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+        await startMFAVerification(
+          resolver: state.resolver,
+          context: context,
+        );
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+        nav.pushReplacementNamed('/profile');
+      },
+    );
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.standard,
+        useMaterial3: true,
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(style: buttonStyle),
+        textButtonTheme: TextButtonThemeData(style: buttonStyle),
+        outlinedButtonTheme: OutlinedButtonThemeData(style: buttonStyle),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      initialRoute: initialRoute,
+      routes: {
+        '/': (context) => SignInScreen(
+              actions: [
+                ForgotPasswordAction((context, email) {
+                  Navigator.pushNamed(
+                    context,
+                    '/forgot-password',
+                    arguments: {'email': email},
+                  );
+                }),
+                VerifyPhoneAction((context, _) {
+                  Navigator.pushNamed(context, '/phone');
+                }),
+                AuthStateChangeAction((context, state) {
+                  final user = switch (state) {
+                    SignedIn(user: final user) => user,
+                    CredentialLinked(user: final user) => user,
+                    UserCreated(credential: final cred) => cred.user,
+                    _ => null,
+                  };
+
+                  switch (user) {
+                    case User(emailVerified: true):
+                      Navigator.pushReplacementNamed(context, '/profile');
+                    // case User(emailVerified: false, email: final String _):
+                    //   Navigator.pushNamed(context, '/verify-email');
+                  }
+                }),
+                mfaAction,
+                EmailLinkSignInAction((context) {
+                  Navigator.pushReplacementNamed(
+                      context, '/email-link-sign-in');
+                }),
+              ],
+              styles: const {
+                EmailFormStyle(signInButtonVariant: ButtonVariant.filled),
+              },
+              // headerBuilder: headerImage('assets/images/flutterfire_logo.png'),
+              // sideBuilder: sideImage('assets/images/flutterfire_logo.png'),
+              subtitleBuilder: (context, action) {
+                final actionText = switch (action) {
+                  AuthAction.signIn => 'Please sign in to continue.',
+                  AuthAction.signUp => 'Please create an account to continue',
+                  _ => throw Exception('Invalid action: $action'),
+                };
+
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text('Welcome to Firebase UI! $actionText.'),
+                );
+              },
+              footerBuilder: (context, action) {
+                final actionText = switch (action) {
+                  AuthAction.signIn => 'signing in',
+                  AuthAction.signUp => 'registering',
+                  _ => throw Exception('Invalid action: $action'),
+                };
+
+                return Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text(
+                      'By $actionText, you agree to our terms and conditions.',
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ),
+                );
+              },
+            ),
+        // '/verify-email': (context) => EmailVerificationScreen(
+        //       // headerBuilder: headerIcon(Icons.verified),
+        //       // sideBuilder: sideIcon(Icons.verified),
+        //       actionCodeSettings: actionCodeSettings,
+        //       actions: [
+        //         EmailVerifiedAction(() {
+        //           Navigator.pushReplacementNamed(context, '/profile');
+        //         }),
+        //         AuthCancelledAction((context) {
+        //           FirebaseUIAuth.signOut(context: context);
+        //           Navigator.pushReplacementNamed(context, '/');
+        //         }),
+        //       ],
+        //     ),
+        '/phone': (context) => PhoneInputScreen(
+              actions: [
+                SMSCodeRequestedAction((context, action, flowKey, phone) {
+                  Navigator.of(context).pushReplacementNamed(
+                    '/sms',
+                    arguments: {
+                      'action': action,
+                      'flowKey': flowKey,
+                      'phone': phone,
+                    },
+                  );
+                }),
+              ],
+              // headerBuilder: headerIcon(Icons.phone),
+              // sideBuilder: sideIcon(Icons.phone),
+            ),
+        '/sms': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments
+              as Map<String, dynamic>?;
+
+          return SMSCodeInputScreen(
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.of(context).pushReplacementNamed('/profile');
+              })
+            ],
+            flowKey: arguments?['flowKey'],
+            action: arguments?['action'],
+            // headerBuilder: headerIcon(Icons.sms_outlined),
+            // sideBuilder: sideIcon(Icons.sms_outlined),
+          );
+        },
+        '/forgot-password': (context) {
+          final arguments = ModalRoute.of(context)?.settings.arguments
+              as Map<String, dynamic>?;
+
+          return ForgotPasswordScreen(
+            email: arguments?['email'],
+            headerMaxExtent: 200,
+            // headerBuilder: headerIcon(Icons.lock),
+            // sideBuilder: sideIcon(Icons.lock),
+          );
+        },
+        '/email-link-sign-in': (context) => EmailLinkSignInScreen(
+              actions: [
+                AuthStateChangeAction<SignedIn>((context, state) {
+                  Navigator.pushReplacementNamed(context, '/');
+                }),
+              ],
+              provider: emailLinkProviderConfig,
+              headerMaxExtent: 200,
+              // headerBuilder: headerIcon(Icons.link),
+              // sideBuilder: sideIcon(Icons.link),
+            ),
+        '/profile': (context) => ProfileScreen(
+              actions: [
+                SignedOutAction((context) {
+                  Navigator.pushReplacementNamed(context, '/');
+                }),
+                mfaAction,
+              ],
+              actionCodeSettings: actionCodeSettings,
+              showMFATile: kIsWeb ||
+                  Theme.of(context).platform == TargetPlatform.iOS ||
+                  Theme.of(context).platform == TargetPlatform.android,
+              showUnlinkConfirmationDialog: true,
+              showDeleteConfirmationDialog: true,
+            ),
+      },
+      title: 'Firebase UI demo',
+      debugShowCheckedModeBanner: false,
+      supportedLocales: const [Locale('en')],
+      localizationsDelegates: [
+        FirebaseUILocalizations.withDefaultOverrides(const LabelOverrides()),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        FirebaseUILocalizations.delegate,
+      ],
     );
   }
 }
